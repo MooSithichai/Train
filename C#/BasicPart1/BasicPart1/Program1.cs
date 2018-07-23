da2c5c2bc68e86f9d35f4419317c81f8e2c74ad7
unknown <sithichai.moo@gmail.com>
2018-07-23 14:26:58 +0700

    HelloWorld

---
+++ C#/BasicPart1/BasicPart1/Program1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicPart1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //ขึ้นบรรทัดใหม่
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
